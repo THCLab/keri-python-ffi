89273a5ea639c4422232e84bcdd37c742e3326d3
Edyta Pawlak <edyta.pawlakk@gmail.com>
2021-03-11 18:29:58 +0100

    Add sign and verify example

--- ffi/python/entity_example.py
+++ ffi/python/entity_example.py
@@ -19,16 +19,53 @@ entity_id = ":".join(["did", "keri", entity.get_prefix()])
 entity.run()
 
 while(True):
-  command = "\trot - update keys \n\tkel - prints key event log \n\tdid <PREFIX> - prints did document of did:keri:<PREFIX> entity \n" 
-  val = input("\nAvailable commands \n" + command)
+  command = """
+  rot - update keys
+  kel - prints key event log
+  diddoc <PREFIX> - prints did document of did:keri:<PREFIX> entity
+  sign <MESSAGE> - sign given message and creates VC
+  verify - verify signature of last signed VC\n\n"""
+  val = input("Available commands:" + command)
 
   if val == "rot":
+    # rotate keys
     entity.update_keys()
     print("Keys updated\n")
   elif val == "kel":
+    # print kel
     print(entity.get_kerl() + "\n")
-  elif val[:3] == "did":
+  elif val[:4] == "sign":
+    # sign message with current key
+    inp = val.split(" ", 1)
+    message = inp[1]
+    signature = entity.sign(message)
+    b64_signature = base64.urlsafe_b64encode(bytes(signature)).decode("utf-8")
+    # create crudential and write it to file
+    # TODO use some better way of sending crudential than the file.
+    crudential = {"issuer": ":".join(["did", "keri", entity.get_prefix()]), "msg": message, "signature": b64_signature}
+    with open('last_crudential', 'w') as file:
+      file.write(json.dumps(crudential))
+    print("Create VC: \n" + json.dumps(crudential, indent=4, sort_keys=True) + "\n")
+  elif val[:6] == "verify":
+    # read crudential from file and verify the signature
+    with open('last_crudential', 'r') as file:
+      crud = file.read()
+    crudential = json.loads(crud)
+    print("Got VC: \n" + json.dumps(crudential, indent=4, sort_keys=True))
+        
+    issuer = crudential['issuer'].split(":")[2]
+    msg = crudential['msg']
+    signature = crudential['signature']
+
+    verification = entity.verify(issuer, msg, signature)
+    if verification:
+      print("Signature is verified\n")
+    else :
+      print("Wrong signature\n")
+  elif val[:6] == "diddoc":
+    # get did document for given prefix
     inp = val.split(" ")
     ddoc = entity.get_did_doc(inp[1])
     formated_ddoc = json.dumps(json.loads(ddoc), indent=4, sort_keys=True)
     print("\n" + "did document: \n" + formated_ddoc + "\n")
+
